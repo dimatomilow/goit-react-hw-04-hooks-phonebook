3f7d1cb260d2b12ae31ed8caa706ccb7d1ca03d8
dimatomilow <masikuspaime@gmail.com>
2022-01-27 20:48:53 +0200

    phonebook final

--- src/App.js
+++ src/App.js
@@ -8,16 +8,29 @@ import Filter from './Component/Filter/Filter';
 
 class App extends Component {
 
-  state = { contacts: [{id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
+  state = { contacts: [],
     filter: ''
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    const contacts = this.state.contacts;
+    if (contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  };
+
+  componentDidMount() {
+    const contact = localStorage.getItem('contacts');
+    const parsedContact = JSON.parse(contact);
+    if (parsedContact) {
+      this.setState({ contacts: parsedContact });
+    }
+
+  };
+
   addForm = (name, number) => {
     const nameToAdd = this.state.contacts.find(
-      contact => contact.name.toLowerCase() === name.toLowerCase(),
+      contact => contact.name.toLowerCase() === name.toLowerCase() && contact.number === number,
     );
     if (nameToAdd) {
       return alert(`${name} is already in contacts.`);
